f2c53231e3fc1e2803a62420e38d2d0be9d69019
robonexx <robertwagar@gmail.com>
2023-02-28 14:05:56 +0100

    changes

--- src/assets/data/pageData.js
+++ src/assets/data/pageData.js
@@ -1,9 +1,3 @@
-import IMG1 from '/public/images/tony_history2.png';
-import IMG2 from '/public/images/tony_history5.png';
-import IMG3 from '/public/images/tony_history7.png';
-import IMG4 from '/public/images/tony_history4.png';
-import IMG5 from '/public/images/tony_history8.png';
-
 const ytVid1 = 'ukJLxTC86aI';
 const ytVid2 = 'IS9m2-fJ4Y0';
 const ytVid3 = '6F2lsPwIYWg';
