1ca3b267a7700587949644881b620931b3d6e8eb
robonexx <robertwagar@gmail.com>
2023-11-06 13:20:31 +0100

    changing about page, working on it

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import IMG from '/public/images/p_lock.jpg';
-import IMG2 from '/public/images/pc2bLogo.png';
+import IMG from '/public/gregskeeter.JPG';
+import IMG2 from '/public/2007.JPG';
 
 // styles
 import styles from './About.module.scss';
@@ -10,22 +10,12 @@ import styles from './About.module.scss';
 const About = () => {
   return (
     <div className={styles.about}>
-      <motion.div
-        className={styles.img_pos}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.6, delay: 0.8 }}
-      >
-        <motion.div>
-          <Image className={styles.img} src={IMG2} alt='pc2b' fill />
-        </motion.div>
-      </motion.div>
       <motion.p
         initial={{ opacity: 0, x: 800, y: 40 }}
-        animate={{ opacity: 1, x:200, y: 40 }}
+        animate={{ opacity: 1, x:100, y: 40 }}
         transition={{ duration: 1.6, delay: 1 }}
       >
-        Founder of the event...
+        Founder of the funkcamp...
       </motion.p>
       <motion.div
         className={styles.img_wrapper}
@@ -33,55 +23,49 @@ const About = () => {
         animate={{ scale: 1 }}
         transition={{ duration: 0.6, delay: 0.8 }}
       >
-        <Image className={styles.img} src={IMG} alt='p lock' fill />
+        <Image className={styles.img} src={IMG} alt='skeeter and greg 2005' fill />
       </motion.div>
       <motion.h4
         initial={{ y: 200, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 1 }}
       >
-        The man himself,{' '}
+        The OG's who was there,{' '}
       </motion.h4>
       <motion.h2
         initial={{ y: 200, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 1 }}
       >
-        P.LOCK - The THUNDER MAN ⚡️!{' '}
+       From The Start ⚡️!{' '}
       </motion.h2>
       <motion.p
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.6, delay: 1.6 }}
       >
-        Battle star ✨, Locker and Choreographer Patrick Pires, co-founding member
-        of The Rockets 🚀, has preserved a high level and remarkable skill in the Art-form of locking.
+        Funkcamp 2005 ✨ Frist ever locking only camp in Sweden and Europe.
+        <br />
+        From there the rest is history, we now have camps all over the world
+        <br />
+        Original idea to camp was from OG Skeeter Rabbit and with the help of Greg Campbellock Jr, 
+        we put the ideas together and set the plans in motion
         <br />
         <br />
-        Originally from Portugal, P started learning dance the age of... He now
-        lives in Paris and is one of the most prominant lockers of this new
-        generation.
-        <br />
-        <br />
-        While dancing with his partner J.Soul he quickly got in to winning
-        several battles and as a Solo locker he is undisputed, won most
-        competitions in the world.
+        While at the sametime the Elite camp started in LA, also original idea from Skeeter and Greg, today many are talking about what and who was first
+        actually not even knowing what was said and planned before everything was set in motion. Still the Vegas camp was a good place for many and sadly not continued.
       </motion.p>
       <p>
-        Last years he has been member and founder of the Rockets and he also has
-        his own event Payed the cost to be the Boss.
+        The past soon 20 years has been remarkable, learning from the pioneers, connecting the youth and future lockers to the pioneers and to be able to learn from some of the sources directly has been an honor, to see some lockers grow from beeing kids at the first camps to being some of the top dancers in the world. 
         <br />
-        Being the only pioneer of Locking to reside in Japan, Tony has trained
-        most Japanese Lockers who have gained international acclaim, but none
-        more prominent than his two sons Yuu and Rei, better known as the Go Go
-        Brothers of today. They have a strong synchronised locking background by
-        which, one can tell, they learned from only the very best - Tony
-        recalls, &apos;When Me, Skeet and Buddy got together in the early
-        70&apos;s we worked on steps over and over again, and that&apos;s the
-        way I taught them&apos;. It doesn&apos;t come as a surprise then that
-        Yuu and Rei started dancing around the same time they started walking.
-        They trained in Locking and Tap Dance from a very early age, performing
-        with their father on numerous occasions throughout Japan.
+        The camp has always had 3 pioneers with roots to the Locking community, the clubs, soul train and yes GOGO Brothers family.
+        Many have been supporting the camp for years now, but without these 3 pillars, OG Skeeter Rabbit, TONY GOGO and Campbellock JR, This camp would have not existed. So Im deeply honered and humbled by the help, continues support and the brother hood you have shown us. 
+        <br />
+        <br />
+        And non of this or the style with the original name Campellocking from the creator Don Campbellock Campbell, this style aka locking, has been spreading around the world and been enjoyed by many. 
+        The fact that we are all a part of an afro-american legacy and it is our duty to pass it on to the new generations. 
+        The Lockers, Campbellock dancers was well known groups in the 70s, and Don was a part of them
+        <br />
         <br />
         Although Campbell can&apos;t lay claim to having the first Locking crew,
         that distinction belongs to Tony GoGo and the Go-Go, and Yo-Yo Brothers,
